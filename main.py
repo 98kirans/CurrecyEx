3d3e6be46cb19a9b46d95a6484cf8823a400cc89
98kirans <bltcc@iup.edu>
2021-12-07 18:37:13 -0500

    #conversion_function

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ class cc():
     def __init__(self,url):
             self.data = requests.get(url).json()
             self.currencies = self.data['rates']
+	def convert(self, from_currency, to_currency, amount): 
+	        initial_amount = amount 
+	        if from_currency != 'EUR' : 
+	            amount = amount / self.currencies[from_currency] 
+	        amount = round(amount * self.currencies[to_currency], 2) 
+	        return amount
