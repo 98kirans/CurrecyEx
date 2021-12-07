f1afca3f9f34124ba26ed0f980a91f913bf7d81e
98kirans <bltcc@iup.edu>
2021-12-07 18:52:49 -0500

    #display_chart

--- main.py
+++ main.py
@@ -60,6 +60,33 @@ converted_amount_field_label = Label(root, text = 'Converted Amount').pack()
 converted_amount_field = Entry(root, width = 15)
 converted_amount_field.pack()
 
+def get_yearly_rates(amount, currency, converted_currency, amount_of_days) :
+	today_date = datetime.datetime.now()
+	date_1year = (today_date - datetime.timedelta(days=1 * amount_of_days))
+	url2=f'https://api.exchangerate.host/timeseries'
+	payload = {'base': currency, 'amount': amount, 'start_date': date_1year.date(), 'end_date': today_date.date()}
+
+	response = requests.get(url2, params=payload)
+	data = response.json()
+
+	currency_history = {}
+	rate_history_array = []
+
+	for item in data['rates']:
+		currency_date = item
+		currency_rate = data['rates'][item][converted_currency]
+		currency_history[currency_date] = [currency_rate]
+		rate_history_array.append(currency_rate)
+
+	pd_data = pd.DataFrame(currency_history).transpose()
+	pd_data.columns = ['Rate']
+	pd.set_option('display.max_rows', None)
+	#print(pd_data)
+	plt.plot(rate_history_array)
+	plt.ylabel(f'{amount} {currency} to {converted_currency}')
+	plt.xlabel('Days')
+	plt.title(f' Current rate for {amount} {currency} to {converted_currency} is {rate_history_array[-1]}')
+	plt.show()
 
 
 root.mainloop()
