7b9e327a7dec886964d4c9c9ae06d1f361368e94
98kirans <bltcc@iup.edu>
2021-12-07 18:58:13 -0500

    #edit_cc_class

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 
+class cc():
+    def __init__(self,url):
+            self.data = requests.get(url).json()
+            self.currencies = self.data['rates']
+
+    def convert(self, from_currency, to_currency, amount): 
+        initial_amount = amount 
+        if from_currency != 'EUR' : 
+            amount = amount / self.currencies[from_currency] 
+        amount = round(amount * self.currencies[to_currency], 2) 
+        return amount
+    	
+root = Tk()
+root.geometry("400x400")
 
 def perf():
 	global amt, from_curr, to_curr
